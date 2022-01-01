cd777bad81131116f159edce2e1e17a88ba585c6
Jadwiga <jadwiga.coumert@gmail.com>
2022-01-01 16:07:59 +0100

    extract playlists logic in user.rb

--- lib/spotify/cli.rb
+++ lib/spotify/cli.rb
@@ -6,41 +6,22 @@ require 'csv'
 
 module Spotify
   class CLI < Thor
-    PLAYLIST_LIMIT = 50
-    desc 'download_playlists', 'Downloads all user\'s playlists into zipped csv'
+    desc 'download_playlists', 'Downloads spotify user\'s playlists in csvs'
 
     def download_playlists
       say('Welcome to the Spotify playlist CLI')
-      user = Spotify::User.default
-      playlists = find_playlists(user)
-      say("User: #{user.display_name} has #{playlists.count} playlists.")
+      user = Spotify::User.new
+      say("User: #{user.spotify_user.display_name} has #{user.playlists.count} playlists.")
       continue = ask('Do you want to continue? (yes/no)')
       return if continue.eql?('no')
 
-      playlists.each_with_index do |playlist, index|
-        say("Exporting playlist n.#{index}: #{playlist.name}...")
+      user.playlists.each_with_index do |playlist, index|
+        say("Exporting playlist n.#{index}: #{playlist.name}")
         Spotify::PlaylistCsv.create!(playlist: playlist)
       end
 
-      say("Your exported playlists can be found in /tmp/playlists")
-    end
-
-    private
-
-    def find_playlists(user)
-      playlists = user.playlists(limit: PLAYLIST_LIMIT)
-      continue = playlists.count.eql?(PLAYLIST_LIMIT)
-      counter = 0
-
-      while continue
-        counter += 1
-        offset = 50 * counter
-        new_playlists = user.playlists(limit: PLAYLIST_LIMIT, offset: offset)
-        playlists.concat(new_playlists)
-        continue = !new_playlists.count.zero? && (playlists.count % PLAYLIST_LIMIT).zero?
-      end
-
-      playlists
+      say('Your exported playlists can be found in /tmp/playlists')
+      say('Thank you for using Spotiplay, come back anytime!')
     end
   end
 end
--- lib/spotify/user.rb
+++ lib/spotify/user.rb
@@ -7,12 +7,37 @@ require 'pry'
 module Spotify
   class User
     class AuthenticationError < StandardError; end
+    PLAYLIST_LIMIT = 50
+    attr_reader :playlists, :spotify_user
 
     def self.default
       RSpotify.authenticate(@@configuration.client_id, @@configuration.client_secret)
       RSpotify::User.find(@@configuration.username)
     rescue RestClient::NotFound => e
       raise AuthenticationError, "Something went wrong in the authentication. Message: #{e.message}"
+    end
+
+    def initialize
+      @spotify_user = Spotify::User.default
+      @playlists = find_playlists
+    end
+
+    private
+
+    def find_playlists
+      playlists = spotify_user.playlists(limit: PLAYLIST_LIMIT)
+      continue = playlists.count.eql?(PLAYLIST_LIMIT)
+      counter = 0
+
+      while continue
+        counter += 1
+        offset = PLAYLIST_LIMIT * counter
+        new_playlists = spotify_user.playlists(limit: PLAYLIST_LIMIT, offset: offset)
+        playlists.concat(new_playlists)
+        continue = !new_playlists.count.zero? && (playlists.count % PLAYLIST_LIMIT).zero?
+      end
+
+      playlists
     end
   end
 end
