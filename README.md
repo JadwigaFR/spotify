77142585d0bdb859543cc3c058c716e57dd0566b
Jadwiga <jadwiga.coumert@gmail.com>
2022-01-01 15:59:19 +0100

    add setup instructions

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Spotify
 
 Spotify CLI to download a spotify user's playlists in csv format.
+
+## Getting started
+- Run the setup script and follow the instructions
+```shell 
+./bin/setup.sh 
+```
+- Add secret key in `.sekrets.key`
+- And voilà! Now you can use the `spotiplay` command from the app folder to use the application.
+
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Installing ruby 2.6..."
+echo "Installing ruby and a ruby version manager.."
 sudo apt update
 sudo apt install -y rbenv
 rbenv install 2.6.6
@@ -18,9 +18,11 @@ if [[ ! -e .sekrets.key ]]; then
   echo > .sekrets.key
 fi
 
-echo "Making spotiplay executable from everywhere in machine..."
 chmod +x exe/spotiplay
 
 echo "Please provide your spotify Username (to be found on: 'https://www.spotify.com/us/account/overview/'"
 read -e -p "Username: " USERNAME
 echo "username: $USERNAME" > config/credentials.yml
+
+mkdir -p tmp/playlists
+
